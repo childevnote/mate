e0099fcf8e4877135f673b4692d38f8cb2c571e2
daniel011125 <daniel011125@gmail.com>
2025-01-22 17:19:51 +0000

    Chore : add metadata in homepage

--- mate-app/app/layout.tsx
+++ mate-app/app/layout.tsx
@@ -1,12 +1,17 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Mate',
+  description: '대학생활의 모든 것, mate와 함께',
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
         <main>{children}</main>
       </body>
