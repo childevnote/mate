c89fe716a578a28645dd0cf8d88280f18c4cd8ea
childevnote <daniel011125@gmail.com>
2026-01-06 18:13:56 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,17 +4,31 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from api.v1.endpoints import community, users, login
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",                      
+    "https://main.d3tpdfp23uq4rz.amplifyapp.com", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,    
+    allow_credentials=True,   
+    allow_methods=["*"],      
+    allow_headers=["*"],      
+)
+
 app.include_router(community.router, prefix="/api/v1/community", tags=["Community"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(login.router, prefix="/api/v1/auth", tags=["auth"])
+
 @app.get("/")
 def health_check():
     return {"message": "FastAPI Server is Running"}
-
 
 handler = Mangum(app)
